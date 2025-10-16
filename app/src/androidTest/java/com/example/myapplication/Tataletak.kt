935a866b7b506b55045fb25419d2692574583268
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-10-16 09:25:48 +0700

    first commit

---
+++ app/src/androidTest/java/com/example/myapplication/Tataletak.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+@Composable
+fun TataLetakColumn(modifier : Modifier) {
+    column(modifier = Modifier.padding(top = 50.dp)) {
+        Text(text = "")
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/coba/coba.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.coba
+
+@composable
+fun TataletakColumn(modifier : Modifier) {
+    column(modifier = Modifier.padding(top = 50.dp)) {
+        Text(text = "")
+    }
+}
