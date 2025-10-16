09150c25c44dc16ae9aea373c8135585299e4303
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-10-16 09:29:45 +0700

    menambahkan padding start pada fungsi tatleatakcolumn

--- app/src/androidTest/java/com/example/myapplication/Tataletak.kt
+++ app/src/androidTest/java/com/example/myapplication/Tataletak.kt
@@ -2,7 +2,7 @@ package com.example.myapplication
 
 @Composable
 fun TataLetakColumn(modifier : Modifier) {
-    column(modifier = Modifier.padding(top = 50.dp)) {
+    column(modifier = Modifier.padding(top = 50.dp, start = 20.dp)) {
         Text(text = "")
     }
 }
